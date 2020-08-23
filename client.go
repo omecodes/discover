905f4c87c3bc925a512f8e08180ea2ee9e503bae
omecodes <omecodes@gmail.com>
2020-08-24 00:30:07 +0200

    Code refactoring - Supports zebou latest version

--- client.go
+++ client.go
@@ -2,31 +2,178 @@ package discover
 
 import (
 	"crypto/tls"
+	"github.com/google/uuid"
+	"github.com/omecodes/common/errors"
+	"github.com/omecodes/common/utils/codec"
 	"github.com/omecodes/common/utils/log"
+	ome "github.com/omecodes/libome"
 	pb2 "github.com/omecodes/libome/proto/service"
 	"github.com/omecodes/zebou"
 	pb "github.com/omecodes/zebou/proto"
+	"strings"
 	"sync"
 )
 
 type msgClient struct {
+	sync.Mutex
+	handlers  map[string]pb2.EventHandler
 	messenger *zebou.Client
-	*registry
-	store *sync.Map
+	store     *sync.Map
 }
 
-func (m *msgClient) handleMessage(msg *pb.SyncMessage) {
-	if m.registry != nil {
-		m.registry.Handle(msg)
+func (m *msgClient) RegisterService(info *pb2.Info) error {
+	encoded, err := codec.Json.Encode(info)
+	if err != nil {
+		log.Info("could not encode service info", log.Err(err))
+		return err
 	}
+
+	err = m.messenger.SendMsg(&pb.SyncMessage{
+		Type:    pb2.EventType_Register.String(),
+		Id:      info.Id,
+		Encoded: encoded,
+	})
+	if err != nil {
+		log.Error("could not send message to server", log.Err(err))
+	}
+	return err
+}
+
+func (m *msgClient) DeregisterService(id string, nodes ...string) error {
+	var encoded []byte
+	msg := &pb.SyncMessage{
+		Id: id,
+	}
+
+	if len(nodes) > 0 {
+		encoded = []byte(strings.Join(nodes, "|"))
+		msg.Encoded = encoded
+		msg.Type = pb2.EventType_DeRegisterNode.String()
+	} else {
+		msg.Type = pb2.EventType_DeRegister.String()
+	}
+
+	err := m.messenger.SendMsg(msg)
+	if err != nil {
+		log.Error("could not send message to server", log.Err(err))
+	}
+	return err
+}
+
+func (m *msgClient) GetService(id string) (*pb2.Info, error) {
+	var info *pb2.Info
+	m.store.Range(func(key, value interface{}) bool {
+		if key == id {
+			info = value.(*pb2.Info)
+			return false
+		}
+		return true
+	})
+	if info == nil {
+		return nil, errors.NotFound
+	}
+	return info, nil
+}
+
+func (m *msgClient) GetNode(id string, nodeId string) (*pb2.Node, error) {
+	info, err := m.GetService(id)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, n := range info.Nodes {
+		if n.Id == nodeId {
+			return n, nil
+		}
+	}
+	return nil, errors.NotFound
+}
+
+func (m *msgClient) Certificate(id string) ([]byte, error) {
+	info, err := m.GetService(id)
+	if err != nil {
+		return nil, err
+	}
+	strCert, found := info.Meta[ome.MetaServiceCertificate]
+	if !found {
+		return nil, errors.NotFound
+	}
+	return []byte(strCert), nil
+}
+
+func (m *msgClient) ConnectionInfo(id string, protocol pb2.Protocol) (*pb2.ConnectionInfo, error) {
+	info, err := m.GetService(id)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, n := range info.Nodes {
+		if protocol == n.Protocol {
+			ci := new(pb2.ConnectionInfo)
+			ci.Address = n.Address
+			strCert, found := info.Meta["certificate"]
+			if !found {
+				return ci, nil
+			}
+			ci.Certificate = []byte(strCert)
+			return ci, nil
+		}
+	}
+
+	return nil, errors.NotFound
+}
+
+func (m *msgClient) RegisterEventHandler(h pb2.EventHandler) string {
+	m.Lock()
+	defer m.Unlock()
+	hid := uuid.New().String()
+	m.handlers[hid] = h
+	return hid
+}
+
+func (m *msgClient) DeregisterEventHandler(id string) {
+	m.Lock()
+	defer m.Unlock()
+	delete(m.handlers, id)
+}
+
+func (m *msgClient) GetOfType(t pb2.Type) ([]*pb2.Info, error) {
+	var result []*pb2.Info
+	m.store.Range(func(key, value interface{}) bool {
+		info := value.(*pb2.Info)
+		if info.Type == t {
+			result = append(result, info)
+		}
+		return true
+	})
+	if len(result) == 0 {
+		return nil, errors.NotFound
+	}
+
+	return result, nil
+}
+
+func (m *msgClient) FirstOfType(t pb2.Type) (*pb2.Info, error) {
+	var info *pb2.Info
+	m.store.Range(func(key, value interface{}) bool {
+		info = value.(*pb2.Info)
+		return info.Type != t
+	})
+	if info == nil {
+		return nil, errors.NotFound
+	}
+	return info, nil
+}
+
+func (m *msgClient) Stop() error {
+	return nil
 }
 
 func NewMSGClient(server string, tlsConfig *tls.Config) *msgClient {
 	c := new(msgClient)
 	c.store = new(sync.Map)
 
-	c.messenger = zebou.Connect(server, tlsConfig, pb.MessageHandlerFunc(c.handleMessage))
-	c.registry = newRegistry(c.store, c.messenger, c.messenger)
+	c.messenger = zebou.Connect(server, tlsConfig)
 	c.messenger.SetConnectionSateHandler(zebou.ConnectionStateHandlerFunc(func(active bool) {
 		if active {
 			c.store.Range(func(key, value interface{}) bool {
--- server.go
+++ server.go
@@ -1,19 +1,26 @@
 package discover
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/omecodes/common/dao/mapping"
 	"github.com/omecodes/common/errors"
 	"github.com/omecodes/common/netx"
 	"github.com/omecodes/common/utils/codec"
-	"github.com/omecodes/common/utils/doer"
 	"github.com/omecodes/common/utils/log"
 	pb2 "github.com/omecodes/libome/proto/service"
 	"github.com/omecodes/zebou"
 	pb "github.com/omecodes/zebou/proto"
 	"net"
+	"path/filepath"
+	"strings"
 	"sync"
 )
 
 type ServerConfig struct {
+	StoreDir     string
 	BindAddress  string
 	CertFilename string
 	KeyFilename  string
@@ -21,80 +28,304 @@ type ServerConfig struct {
 
 type msgServer struct {
 	sync.Mutex
-	listener      net.Listener
-	stopper       doer.Stopper
-	store         *sync.Map
-	eventHandlers map[string]pb2.EventHandler
+	handlers map[string]pb2.EventHandler
+	listener net.Listener
+	hub      *zebou.Hub
+	store    mapping.DoubleMap
 }
 
-func (s *msgServer) Handle(msg *pb.SyncMessage) error {
+func (s *msgServer) NewClient(ctx context.Context, info *zebou.PeerInfo) {
+	peer := zebou.Peer(ctx)
+	if peer != nil {
+		log.Info("new client connected", log.Field("conn_id", peer.ID), log.Field("addr", peer.Address))
+	} else {
+		log.Info("new client connected")
+	}
+}
+
+func (s *msgServer) ClientQuit(ctx context.Context, peer *zebou.PeerInfo) {
+	if peer != nil {
+		log.Info("client disconnected", log.Field("conn_id", peer.ID), log.Field("addr", peer.Address))
+		c, err := s.store.GetForFirst(peer.ID)
+		if err != nil {
+			log.Error("failed to get registered nodes", log.Field("conn_id", peer.ID), log.Field("addr", peer.Address))
+			return
+		}
+
+		for c.HasNext() {
+			var info pb2.Info
+			id, err := c.Next(&info)
+			if err != nil {
+				log.Error("failed to parse service info", log.Err(err))
+				return
+			}
+
+			encoded, err := codec.Json.Encode(info)
+			if err != nil {
+				log.Error("failed to encode service info", log.Err(err))
+				return
+			}
+
+			s.hub.Broadcast(ctx, &pb.SyncMessage{
+				Type:    pb2.EventType_DeRegisterNode.String(),
+				Id:      id,
+				Encoded: encoded,
+			})
+		}
+
+	} else {
+		log.Info("client disconnected")
+	}
+}
+
+func (s *msgServer) OnMessage(ctx context.Context, msg *pb.SyncMessage) {
+	peer := zebou.Peer(ctx)
+	go s.hub.Broadcast(ctx, msg)
+
 	switch msg.Type {
 	case pb2.EventType_Update.String(), pb2.EventType_Register.String():
 		info := new(pb2.Info)
 		err := codec.Json.Decode(msg.Encoded, info)
 		if err != nil {
-			return err
+			log.Error("failed to decode service info from message payload", log.Err(err))
+			return
 		}
-		s.store.Store(msg.Id, info)
+
+		err = s.store.Set(peer.ID, msg.Id, info)
+		if err != nil {
+			log.Error("failed to store service info", log.Err(err))
+			return
+		}
+
+		err = zebou.Broadcast(ctx, msg)
+		if err != nil {
+			log.Error("failed to broadcast service info", log.Err(err))
+			return
+		}
 
 		event := &pb2.Event{
-			ServiceId: msg.Type,
+			ServiceId: msg.Id,
 			Info:      info,
 		}
-		if pb2.EventType_Register.String() == msg.Type {
-			event.Type = pb2.EventType_Register
-		} else {
-			event.Type = pb2.EventType_Update
-		}
+		event.Type = pb2.EventType(pb2.EventType_value[msg.Type])
+		s.notifyEvent(event)
 
 	case pb2.EventType_DeRegister.String():
-		s.store.Delete(msg.Id)
+		err := s.store.Delete(peer.ID, msg.Id)
+		log.Error("could not delete service info", log.Err(err), log.Field("service", msg.Id))
+
+		s.notifyEvent(&pb2.Event{
+			Type:      pb2.EventType_DeRegister,
+			ServiceId: msg.Id,
+		})
 
 	case pb2.EventType_DeRegisterNode.String():
-		o, ok := s.store.Load(msg.Id)
-		if ok {
-			info := o.(*pb2.Info)
-			nodeId := string(msg.Encoded)
-			var newNodes []*pb2.Node
-			for _, node := range info.Nodes {
-				if node.Id != nodeId {
-					newNodes = append(newNodes, node)
-				}
-			}
-			info.Nodes = newNodes
-			s.store.Store(msg.Id, info)
+		var info pb2.Info
+
+		err := s.store.Get(peer.ID, msg.Id, &info)
+		if err != nil {
+			log.Error("failed to read service info", log.Err(err), log.Field("service", msg.Id))
+			return
 		}
+
+		nodeId := string(msg.Encoded)
+		var newNodes []*pb2.Node
+		for _, node := range info.Nodes {
+			if node.Id != nodeId {
+				newNodes = append(newNodes, node)
+			}
+		}
+
+		info.Nodes = newNodes
+		err = s.store.Set(peer.ID, msg.Id, info)
+		if err != nil {
+			log.Error("failed to update service info", log.Err(err), log.Field("service", msg.Id))
+			return
+		}
+
+		s.notifyEvent(&pb2.Event{
+			Type:      pb2.EventType_DeRegisterNode,
+			ServiceId: msg.Id,
+		})
 
 	default:
 		log.Info("received unsupported msg type", log.Field("type", msg.Type))
-		return errors.NotSupported
+	}
+}
+
+func (s *msgServer) RegisterService(i *pb2.Info) error {
+	encoded, err := codec.Json.Encode(i)
+	if err != nil {
+		return err
+	}
+	msg := &pb.SyncMessage{
+		Type:    pb2.EventType_Register.String(),
+		Id:      i.Id,
+		Encoded: encoded,
+	}
+	s.hub.Broadcast(context.Background(), msg)
+
+	go s.notifyEvent(&pb2.Event{
+		Type:      pb2.EventType_Register,
+		ServiceId: i.Id,
+		Info:      i,
+	})
+	return nil
+}
+
+func (s *msgServer) DeregisterService(id string, nodes ...string) error {
+	msg := &pb.SyncMessage{
+		Id: id,
+	}
+
+	if len(nodes) > 0 {
+		encoded := []byte(strings.Join(nodes, "|"))
+		msg.Encoded = encoded
+		msg.Type = pb2.EventType_DeRegisterNode.String()
+		s.hub.Broadcast(context.Background(), msg)
+		ev := &pb2.Event{
+			Type:      pb2.EventType_DeRegisterNode,
+			ServiceId: fmt.Sprintf("%s:%s", id, encoded),
+		}
+		go s.notifyEvent(ev)
+
+	} else {
+		msg.Type = pb2.EventType_DeRegister.String()
+		s.hub.Broadcast(context.Background(), msg)
+		ev := &pb2.Event{
+			Type:      pb2.EventType_DeRegister,
+			ServiceId: id,
+		}
+		go s.notifyEvent(ev)
 	}
 	return nil
 }
 
-func (s *msgServer) State() ([]*pb.SyncMessage, error) {
-	var msgs []*pb.SyncMessage
-	s.store.Range(func(key, value interface{}) bool {
-		info := value.(*pb2.Info)
-		encoded, _ := codec.Json.Encode(info)
-		msgs = append(msgs, &pb.SyncMessage{Id: info.Id, Type: pb2.EventType_Register.String(), Encoded: encoded})
-		return true
-	})
-	return msgs, nil
+func (s *msgServer) GetService(id string) (*pb2.Info, error) {
+	var info pb2.Info
+	err := s.store.Get("local", id, &info)
+	return &info, err
 }
 
-func (s *msgServer) Invalidate(id string) error {
-	s.store.Delete(id)
-	return nil
+func (s *msgServer) GetNode(id string, nodeName string) (*pb2.Node, error) {
+	info, err := s.GetService(id)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, node := range info.Nodes {
+		if node.Id == nodeName {
+			return node, nil
+		}
+	}
+
+	return nil, errors.NotFound
 }
 
-func (s *msgServer) SendMsg(msg *pb.SyncMessage) error {
-	return s.Handle(msg)
+func (s *msgServer) Certificate(id string) ([]byte, error) {
+	info, err := s.GetService(id)
+	if err != nil {
+		return nil, err
+	}
+
+	strCert, found := info.Meta[pb2.MetaServiceCertificate]
+	if !found {
+		return nil, errors.NotFound
+	}
+	return []byte(strCert), nil
+}
+
+func (s *msgServer) ConnectionInfo(id string, protocol pb2.Protocol) (*pb2.ConnectionInfo, error) {
+	info, err := s.GetService(id)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, n := range info.Nodes {
+		if protocol == n.Protocol {
+			ci := new(pb2.ConnectionInfo)
+			ci.Address = n.Address
+			strCert, found := info.Meta["certificate"]
+			if !found {
+				return ci, nil
+			}
+			ci.Certificate = []byte(strCert)
+			return ci, nil
+		}
+	}
+
+	return nil, errors.NotFound
+}
+
+func (s *msgServer) RegisterEventHandler(h pb2.EventHandler) string {
+	s.Lock()
+	defer s.Unlock()
+	hid := uuid.New().String()
+	s.handlers[hid] = h
+	return hid
+}
+
+func (s *msgServer) DeregisterEventHandler(hid string) {
+	s.Lock()
+	defer s.Unlock()
+	delete(s.handlers, hid)
+}
+
+func (s *msgServer) GetOfType(t pb2.Type) ([]*pb2.Info, error) {
+	var msgList []*pb2.Info
+	c, err := s.store.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	for c.HasNext() {
+		var info pb2.Info
+		err := c.Next(&info)
+		if err != nil {
+			return msgList, err
+		}
+
+		if info.Type == t {
+			msgList = append(msgList, nil)
+		}
+	}
+	return msgList, nil
+}
+
+func (s *msgServer) FirstOfType(t pb2.Type) (*pb2.Info, error) {
+	c, err := s.store.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	for c.HasNext() {
+		var info pb2.Info
+		err := c.Next(&info)
+		if err != nil {
+			return nil, err
+		}
+
+		if info.Type == t {
+			return &info, nil
+		}
+	}
+	return nil, errors.NotFound
 }
 
 func (s *msgServer) Stop() error {
-	_ = s.stopper.Stop()
+	_ = s.hub.Stop()
 	return s.listener.Close()
+}
+
+func (s *msgServer) notifyEvent(e *pb2.Event) {
+	s.Lock()
+	defer s.Unlock()
+
+	for _, h := range s.handlers {
+		h.Handle(e)
+	}
 }
 
 func Serve(configs *ServerConfig) (pb2.Registry, error) {
@@ -110,14 +341,26 @@ func Serve(configs *ServerConfig) (pb2.Registry, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	log.Info("[discovery] starting gRPC server", log.Field("at", s.listener.Addr()))
 
-	s.store = &sync.Map{}
-	s.eventHandlers = map[string]pb2.EventHandler{}
-	s.stopper, err = zebou.Serve(s.listener, s)
+	db, err := sql.Open("sqlite3", filepath.Join(configs.StoreDir, "registry.db"))
+	if err != nil {
+		log.Error("could not open registry database", log.Err(err))
+		return nil, err
+	}
+
+	s.store, err = mapping.NewSQL("sqlite", db, "reg", codec.Json)
 	if err != nil {
 		return nil, err
 	}
 
-	return newRegistry(s.store, s, s), nil
+	s.hub, err = zebou.Serve(s.listener, s)
+	if err != nil {
+		return nil, err
+	}
+
+	s.handlers = map[string]pb2.EventHandler{}
+
+	return s, nil
 }
